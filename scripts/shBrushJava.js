63e94fd99393602f31ed9f13bfae9791367e1963
steward-fu <steward.fu@gmail.com>
2020-10-27 17:24:39 +0800

    update program pages

--- scripts/shBrushJava.js
+++ scripts/shBrushJava.js
@@ -16,42 +16,42 @@
  */
 ;(function()
 {
-	// CommonJS
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+  // CommonJS
+  typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-	function Brush()
-	{
-		var keywords =	'abstract assert boolean break byte case catch char class const ' +
-						'continue default do double else enum extends ' +
-						'false final finally float for goto if implements import ' +
-						'instanceof int interface long native new null ' +
-						'package private protected public return ' +
-						'short static strictfp super switch synchronized this throw throws true ' +
-						'transient try void volatile while';
+  function Brush()
+  {
+    var my_type = 'Applet Graphics';
+    var my_constant = 'java.awt.Graphics java.applet.Applet';
+    var my_keyword = 'abstract assert boolean break byte case catch char class const continue default do double else enum extends false final finally float for goto if implements import instanceof int interface long native new null package private protected public return short static strictfp super switch synchronized this throw throws true transient try void volatile while';
+    var my_function = 'paint drawString';
 
-		this.regexList = [
-			{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' },		// one line comments
-			{ regex: /\/\*([^\*][\s\S]*)?\*\//gm,						css: 'comments' },	 	// multiline comments
-			{ regex: /\/\*(?!\*\/)\*[\s\S]*?\*\//gm,					css: 'preprocessor' },	// documentation comments
-			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },		// strings
-			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// strings
-			{ regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gi,				css: 'value' },			// numbers
-			{ regex: /(?!\@interface\b)\@[\$\w]+\b/g,					css: 'color1' },		// annotation @anno
-			{ regex: /\@interface\b/g,									css: 'color2' },		// @interface keyword
-			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }		// java keyword
-			];
+    this.regexList = [
+      { regex: SyntaxHighlighter.regexLib.singleLineCComments, css: 'comments' },
+      { regex: /\/\*([^\*][\s\S]*)?\*\//gm, css: 'comments' },
+      { regex: /\/\*(?!\*\/)\*[\s\S]*?\*\//gm, css: 'preprocessor' },
+      { regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
+      { regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
+      { regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gi, css: 'value' },
+      { regex: /(?!\@interface\b)\@[\$\w]+\b/g, css: 'color1' },
+      { regex: /\@interface\b/g, css: 'color2' },
+      { regex: new RegExp(this.getKeywords(my_type),'gm'), css:'my_type bold'},
+      { regex: new RegExp(this.getKeywords(my_keyword),'gm'), css:'my_keyword bold'},
+      { regex: new RegExp(this.getKeywords(my_constant),'gm'), css:'my_constant bold'},
+      { regex: new RegExp(this.getKeywords(my_function),'gm'), css:'my_function bold'},
+      ];
 
-		this.forHtmlScript({
-			left	: /(&lt;|<)%[@!=]?/g, 
-			right	: /%(&gt;|>)/g 
-		});
-	};
+    this.forHtmlScript({
+      left  : /(&lt;|<)%[@!=]?/g, 
+      right  : /%(&gt;|>)/g 
+    });
+  };
 
-	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases	= ['java'];
+  Brush.prototype  = new SyntaxHighlighter.Highlighter();
+  Brush.aliases  = ['java'];
 
-	SyntaxHighlighter.brushes.Java = Brush;
+  SyntaxHighlighter.brushes.Java = Brush;
 
-	// CommonJS
-	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+  // CommonJS
+  typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
 })();
