2207f90f7ee518ddcb651a726fe2a661b9ad7cee
steward-fu <steward.fu@gmail.com>
2021-04-30 22:32:23 -0400

    init

--- gen_sitemap.py
+++ gen_sitemap.py
@@ -12,9 +12,11 @@ f.write('      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n')
 f.write('      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9\n')
 f.write('            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n')
 
+cnt = 0
 now = datetime.now().isoformat()
 r = commands.getoutput('find . -name "*htm"').split('\n')
 for x in r:
+  cnt = cnt + 1
   n = '  <loc>https://steward-fu.github.io/website/' + x.replace('./', '') + '</loc>\n'
 
   f.write('<url>\n')
@@ -24,4 +26,5 @@ for x in r:
   f.write('</url>\n')
 
 f.close()
+print 'generated {} html'.format(cnt)
 print 'task done!'
