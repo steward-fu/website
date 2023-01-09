b4411e9d3be26ea2918d70dd5f823cd77c60e01e
steward-fu <steward.fu@gmail.com>
2023-01-08 19:47:54 -0500

    Rust An example to show how to use the method of Monte Carlo to calcuate PI.

--- scripts/shBrushRust.js
+++ scripts/shBrushRust.js
@@ -18,10 +18,10 @@
  typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
   function Brush() {
-    var my_constant = 'TEST';
+    var my_constant = 'MAX';
     var my_keyword =  'pub if use struct else Some while async for fn impl as return in let None mut static';
-    var my_type = 'response routing extract Extension sqlx anyhow Result String sqlite SqlitePool serde actix_web web u64 MyStruct App HttpResponse HttpServer Deserialize int Option Html Box ListNode usize i32 u32 std net SocketAddr Router axum Server str Solution Vec vec';
-    var my_function = 'post_create post_read post_update post_delete to_string get connect query fetch_all sort_list main push pop to post get_index post_test run expect Ok content_type body Form format sort as_mut unwrap len println route handler from bind serve await into_make_service as_ref is_none new sort_array';
+    var my_type = 'response gen f64 rand Rng routing extract Extension sqlx anyhow Result String sqlite SqlitePool serde actix_web web u64 MyStruct App HttpResponse HttpServer Deserialize int Option Html Box ListNode usize i32 u32 std net SocketAddr Router axum Server str Solution Vec vec';
+    var my_function = 'thread_rng pow _pi post_create post_read post_update post_delete to_string get connect query fetch_all sort_list main push pop to post get_index post_test run expect Ok content_type body Form format sort as_mut unwrap len println route handler from bind serve await into_make_service as_ref is_none new sort_array';
 
     this.regexList = [
       {regex: /^ *#.*/gm, css: 'my_preprocessor'},
