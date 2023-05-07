46d6ab0cc75bbc88d2f61125a7bdee3b6b1e6661
steward-fu <steward.fu@gmail.com>
2023-05-07 00:34:19 -0400

    OpenSCAD How to use minkowski.

--- scripts/shBrushScad.js
+++ scripts/shBrushScad.js
@@ -21,7 +21,7 @@
     var my_constant = 'true false';
     var my_keyword =  'auto convexity module center for if else';
     var my_type = 'int';
-    var my_function = 'color resize meniscus cube_round_corner echo difference translate rotate hull sphere cube cylinder union';
+    var my_function = 'minkowski color resize meniscus cube_round_corner echo difference translate rotate hull sphere cube cylinder union';
 
     this.regexList = [
       {regex: /^ *#.*/gm, css: 'my_preprocessor'},
