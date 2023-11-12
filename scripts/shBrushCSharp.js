2d2a162649a79fbca6c6bb60fc5442e319c766f6
steward-fu <steward.fu@gmail.com>
2023-11-12 07:19:15 -0500

    C# Added a hello example.

--- scripts/shBrushCSharp.js
+++ scripts/shBrushCSharp.js
@@ -14,52 +14,30 @@
  * @license
  * Dual licensed under the MIT and GPL licenses.
  */
-;(function()
-{
-	// CommonJS
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+;(function() {
+ typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-	function Brush()
-	{
-		var keywords =	'abstract as base bool break byte case catch char checked class const ' +
-						'continue decimal default delegate do double else enum event explicit ' +
-						'extern false finally fixed float for foreach get goto if implicit in int ' +
-						'interface internal is lock long namespace new null object operator out ' +
-						'override params private protected public readonly ref return sbyte sealed set ' +
-						'short sizeof stackalloc static string struct switch this throw true try ' +
-						'typeof uint ulong unchecked unsafe ushort using virtual void while';
+  function Brush() {
+    var my_constant = 'System Console';
+    var my_keyword = 'abstract as base bool break byte case catch char checked class const continue decimal default delegate do double else enum event explicit extern false finally fixed float for foreach get goto if implicit in int interface internal is lock long namespace new null object operator out override params private protected public readonly ref return sbyte sealed set short sizeof stackalloc static string struct switch this throw true try typeof uint ulong unchecked unsafe ushort using virtual void while';
+    var my_type = 'test';
+    var my_function = 'WriteLine Main';
 
-		function fixComments(match, regexInfo)
-		{
-			var css = (match[0].indexOf("///") == 0)
-				? 'color1'
-				: 'comments'
-				;
-			
-			return [new SyntaxHighlighter.Match(match[0], match.index, css)];
-		}
+    this.regexList = [
+      {regex: /^ *#.*/gm, css: 'my_preprocessor'},
+      {regex: SyntaxHighlighter.regexLib.doubleQuotedString, css:'string'},
+      {regex: SyntaxHighlighter.regexLib.singleQuotedString, css:'string'},
+      {regex: SyntaxHighlighter.regexLib.multiLineCComments, css:'my_comment'},
+      {regex: SyntaxHighlighter.regexLib.singleLineCComments, css:'my_comment'},
+      {regex: new RegExp(this.getKeywords(my_type),'gm'), css:'my_type bold'},
+      {regex: new RegExp(this.getKeywords(my_keyword),'gm'), css:'my_keyword bold'},
+      {regex: new RegExp(this.getKeywords(my_constant),'gm'), css:'my_constant bold'},
+      {regex: new RegExp(this.getKeywords(my_function),'gm'), css:'my_function bold'},
+    ];
+  };
 
-		this.regexList = [
-			{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	func : fixComments },		// one line comments
-			{ regex: SyntaxHighlighter.regexLib.multiLineCComments,		css: 'comments' },			// multiline comments
-			{ regex: /@"(?:[^"]|"")*"/g,								css: 'string' },			// @-quoted strings
-			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },			// strings
-			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },			// strings
-			{ regex: /^\s*#.*/gm,										css: 'preprocessor' },		// preprocessor tags like #region and #endregion
-			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' },			// c# keyword
-			{ regex: /\bpartial(?=\s+(?:class|interface|struct)\b)/g,	css: 'keyword' },			// contextual keyword: 'partial'
-			{ regex: /\byield(?=\s+(?:return|break)\b)/g,				css: 'keyword' }			// contextual keyword: 'yield'
-			];
-		
-		this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
-	};
-
-	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases	= ['c#', 'c-sharp', 'csharp'];
-
-	SyntaxHighlighter.brushes.CSharp = Brush;
-
-	// CommonJS
-	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+  Brush.prototype  = new SyntaxHighlighter.Highlighter();
+  Brush.aliases  = ['c#', 'csharp'];
+  SyntaxHighlighter.brushes.Cpp = Brush;
+  typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
 })();
-
