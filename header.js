d1c8abb4ea07a2b8b9fd3973bfb676b6edb233d7
steward-fu <steward.fu@gmail.com>
2024-02-15 22:50:00 -0500

    Removed old header

--- header.js
+++
@@ -1,2 +0,0 @@
-var content='<div id=\"header\"><a href="https://steward-fu.github.io/website/index.htm"><h1><span class=\"big darkBrown\">S</span>teward</h1></a><h2><span class=\"highlight\">分享是一種喜悅、更是一種幸福</span></h2></div>';
-document.write(content);
