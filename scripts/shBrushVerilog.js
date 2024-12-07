f6a5f6ad8ab87204fb6c0c47927fd9c7c091407f
steward-fu <steward.fu@gmail.com>
2024-12-07 12:32:07 +0800

    Lichee Tang Refined.

--- scripts/shBrushVerilog.js
+++ scripts/shBrushVerilog.js
@@ -15,29 +15,29 @@
  * Dual licensed under the MIT and GPL licenses.
  */
 ;(function() {
- typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-  function Brush() {
-    var my_constant = 'posedge LOCATION';
-    var my_keyword =  'module begin end if assign endmodule';
-    var my_type = 'reg input output wire parameter';
-    var my_function = 'main always initial set_pin_assignment';
+function Brush() {
+var my_constant = 'posedge LOCATION negedge';
+var my_keyword = 'module begin end if assign endmodule or else';
+var my_type = 'reg input output wire parameter';
+var my_function = 'main always initial set_pin_assignment';
 
-    this.regexList = [
-      {regex: /^ *#.*/gm, css: 'my_preprocessor'},
-      {regex: SyntaxHighlighter.regexLib.doubleQuotedString, css:'string'},
-      {regex: SyntaxHighlighter.regexLib.singleQuotedString, css:'string'},
-      {regex: SyntaxHighlighter.regexLib.multiLineCComments, css:'my_comment'},
-      {regex: SyntaxHighlighter.regexLib.singleLineCComments, css:'my_comment'},
-      {regex: new RegExp(this.getKeywords(my_type),'gm'), css:'my_type bold'},
-      {regex: new RegExp(this.getKeywords(my_keyword),'gm'), css:'my_keyword bold'},
-      {regex: new RegExp(this.getKeywords(my_constant),'gm'), css:'my_constant bold'},
-      {regex: new RegExp(this.getKeywords(my_function),'gm'), css:'my_function bold'},
-    ];
-  };
+this.regexList = [
+{regex: /^ *#.*/gm, css: 'my_preprocessor'},
+{regex: SyntaxHighlighter.regexLib.doubleQuotedString, css:'string'},
+{regex: SyntaxHighlighter.regexLib.singleQuotedString, css:'string'},
+{regex: SyntaxHighlighter.regexLib.multiLineCComments, css:'my_comment'},
+{regex: SyntaxHighlighter.regexLib.singleLineCComments, css:'my_comment'},
+{regex: new RegExp(this.getKeywords(my_type),'gm'), css:'my_type bold'},
+{regex: new RegExp(this.getKeywords(my_keyword),'gm'), css:'my_keyword bold'},
+{regex: new RegExp(this.getKeywords(my_constant),'gm'), css:'my_constant bold'},
+{regex: new RegExp(this.getKeywords(my_function),'gm'), css:'my_function bold'},
+];
+};
 
-  Brush.prototype  = new SyntaxHighlighter.Highlighter();
-  Brush.aliases  = ['verilog'];
-  SyntaxHighlighter.brushes.Verilog = Brush;
-  typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+Brush.prototype  = new SyntaxHighlighter.Highlighter();
+Brush.aliases  = ['verilog'];
+SyntaxHighlighter.brushes.Verilog = Brush;
+typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
 })();
