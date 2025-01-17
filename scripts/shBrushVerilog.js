9bea3c6755f4b8e4f8ca6996cdabbeb9aed9df94
steward-fu <steward.fu@gmail.com>
2025-01-17 17:46:53 +0800

    STEP-MXO2 How to play simulator.

--- scripts/shBrushVerilog.js
+++ scripts/shBrushVerilog.js
@@ -20,8 +20,8 @@ typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHig
 function Brush() {
 var my_constant = 'posedge LOCATION negedge uart_bus baud baud_rx bps_para uart_rx uart_tx baud_tx';
 var my_keyword = 'module begin end if assign endmodule or else';
-var my_type = 'reg input output wire parameter';
-var my_function = 'main always initial set_pin_assignment';
+var my_type = 'integer reg input output wire parameter';
+var my_function = 'monitor dumpfile dumpvars finish main always initial set_pin_assignment';
 
 this.regexList = [
 {regex: /^ *#.*/gm, css: 'my_preprocessor'},
