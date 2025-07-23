532a304fda7de6e7095cf05bcfb0e111ac9951ef
steward-fu <steward.fu@gmail.com>
2025-07-23 02:49:27 -0400

    GNU Added websocket example.

--- scripts/shBrushXml.js
+++ scripts/shBrushXml.js
@@ -16,54 +16,63 @@
  */
 ;(function()
 {
-	// CommonJS
-	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+// CommonJS
+typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-	function Brush()
-	{
-		function process(match, regexInfo)
-		{
-			var constructor = SyntaxHighlighter.Match,
-				code = match[0],
-				tag = new XRegExp('(&lt;|<)[\\s\\/\\?]*(?<name>[:\\w-\\.]+)', 'xg').exec(code),
-				result = []
-				;
-		
-			if (match.attributes != null) 
-			{
-				var attributes,
-					regex = new XRegExp('(?<name> [\\w:\\-\\.]+)' +
-										'\\s*=\\s*' +
-										'(?<value> ".*?"|\'.*?\'|\\w+)',
-										'xg');
+function Brush()
+{
+    var my_const = 'DOCTYPE';
+    var my_type = 'WebSocket document console';
+    var my_keyword = 'var new function html';
+    var my_function = 'update getElementById onclose onopen onmessage log';
 
-				while ((attributes = regex.exec(code)) != null) 
-				{
-					result.push(new constructor(attributes.name, match.index + attributes.index, 'color1'));
-					result.push(new constructor(attributes.value, match.index + attributes.index + attributes[0].indexOf(attributes.value), 'string'));
-				}
-			}
+    function process(match, regexInfo)
+    {
+        var constructor = SyntaxHighlighter.Match,
+            code = match[0],
+            tag = new XRegExp('(&lt;|<)[\\s\\/\\?]*(?<name>[:\\w-\\.]+)', 'xg').exec(code),
+            result = []
+            ;
+    
+        if (match.attributes != null) 
+        {
+            var attributes,
+                regex = new XRegExp('(?<name> [\\w:\\-\\.]+)' +
+                                    '\\s*=\\s*' +
+                                    '(?<value> ".*?"|\'.*?\'|\\w+)',
+                                    'xg');
 
-			if (tag != null)
-				result.push(
-					new constructor(tag.name, match.index + tag[0].indexOf(tag.name), 'keyword')
-				);
+            while ((attributes = regex.exec(code)) != null) 
+            {
+                result.push(new constructor(attributes.name, match.index + attributes.index, 'color1'));
+                result.push(new constructor(attributes.value, match.index + attributes.index + attributes[0].indexOf(attributes.value), 'string'));
+            }
+        }
 
-			return result;
-		}
-	
-		this.regexList = [
-			{ regex: new XRegExp('(\\&lt;|<)\\!\\[[\\w\\s]*?\\[(.|\\s)*?\\]\\](\\&gt;|>)', 'gm'),			css: 'color2' },	// <![ ... [ ... ]]>
-			{ regex: SyntaxHighlighter.regexLib.xmlComments,												css: 'comments' },	// <!-- ... -->
-			{ regex: new XRegExp('(&lt;|<)[\\s\\/\\?]*(\\w+)(?<attributes>.*?)[\\s\\/\\?]*(&gt;|>)', 'sg'), func: process }
-		];
-	};
+        if (tag != null)
+            result.push(
+                new constructor(tag.name, match.index + tag[0].indexOf(tag.name), 'keyword')
+            );
 
-	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases	= ['xml', 'xhtml', 'xslt', 'html'];
+        return result;
+    }
+    
+this.regexList = [
+{ regex: new XRegExp('(\\&lt;|<)\\!\\[[\\w\\s]*?\\[(.|\\s)*?\\]\\](\\&gt;|>)', 'gm'), css: 'color2' },    // <![ ... [ ... ]]>
+{ regex: SyntaxHighlighter.regexLib.xmlComments, css: 'comments' },    // <!-- ... -->
+{ regex: new XRegExp('(&lt;|<)[\\s\\/\\?]*(\\w+)(?<attributes>.*?)[\\s\\/\\?]*(&gt;|>)', 'sg'), func: process },
+{ regex: new RegExp(this.getKeywords(my_const),'gm'), css:'my_const bold'},
+{ regex: new RegExp(this.getKeywords(my_type),'gm'), css:'my_type bold'},
+{ regex: new RegExp(this.getKeywords(my_keyword),'gm'), css:'my_keyword bold'},
+{ regex: new RegExp(this.getKeywords(my_function),'gm'), css:'my_function bold'}
+];
+};
 
-	SyntaxHighlighter.brushes.Xml = Brush;
+Brush.prototype = new SyntaxHighlighter.Highlighter();
+Brush.aliases = ['xml', 'xhtml', 'xslt', 'html'];
 
-	// CommonJS
-	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+SyntaxHighlighter.brushes.Xml = Brush;
+
+// CommonJS
+typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
 })();
