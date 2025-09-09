48ba671097588d976d99da8216430c355d614aed
steward-fu <steward.fu@gmail.com>
2025-09-09 23:06:50 +0800

    GGM Refined.

--- update.py
+++ update.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-MAGIC = '<!-- Steward-Fu -->'
-
 def patch_html(path):
     if path.endswith('.htm') == False:
         return
@@ -11,24 +9,74 @@ def patch_html(path):
     content = f.readlines()
     f.close()
 
-    chk = content[0].replace('\n', '').replace('\r', '\n')
-    if chk == MAGIC:
-        print('failure, {}'.format(path))
-        return
-
     f = open(path, 'w', encoding='utf-8-sig')
-    f.write(MAGIC + '\n')
-
     start_write = False
     for c in content:
         t = c.replace('\r', '').replace('\n', '')
-        t = t.replace('<br>', '<br />')
-        t = t.replace('class="maxSize">', 'class="maxSize" />')
-        t = t.replace('<hr size="1">', '<hr size="1" />')
         if t == '<div id="page">':
-            f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' + '\n');
-            f.write('<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">' + '\n');
-            f.write('<head><script type="text/javascript" src="../page.js"></script></head>' + '\n');
+            f.write('<!--' + '\n');
+            f.write(' ____________________________________________________________' + '\n');
+            f.write('|                                                            |' + '\n');
+            f.write('|    DESIGN + Pat Heard { http://fullahead.org }             |' + '\n');
+            f.write('|      DATE + 2006.09.12                                     |' + '\n');
+            f.write('| COPYRIGHT + Free use if this notice is kept in place.      |' + '\n');
+            f.write('|____________________________________________________________|' + '\n');
+            f.write('-->' + '\n');
+            f.write('\n');
+            f.write('<head>' + '\n');
+            f.write('<title>司徒的教學網站</title>' + '\n');
+            f.write('<meta http-equiv="content-type" content="text/html; charset=UTF-8" />' + '\n');
+            f.write('<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />' + '\n');
+            f.write('<meta name="author" content="fullahead.org" />' + '\n');
+            f.write('<meta name="keywords" content="XHTML, CSS, template, FullAhead" />' + '\n');
+            f.write('<meta name="description" content="A valid, XHTML 1.0 template" />' + '\n');
+            f.write('<meta name="robots" content="index, follow, noarchive" />' + '\n');
+            f.write('<meta name="googlebot" content="noarchive" />' + '\n');
+            f.write('<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, minimum-scale=0.1, maximum-scale=10.0" />' + '\n');
+            f.write('\n');
+            f.write('<link rel="shortcut icon" href="../website.ico" />' + '\n');
+            f.write('<link rel="stylesheet" type="text/css" href="../styles/shCore.css" />' + '\n');
+            f.write('<link rel="stylesheet" type="text/css" href="../styles/shThemeDefault.css" />' + '\n');
+            f.write('<link rel="stylesheet" type="text/css" href="../css/html.css" media="screen" />' + '\n');
+            f.write('<link rel="stylesheet" type="text/css" href="../css/layout.css" media="screen" />' + '\n');
+            f.write('\n');
+            f.write('<script type="text/javascript" src="../scripts/shCore.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushAppleScript.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushAS3.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushAsm.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushBash.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushColdFusion.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushCpp.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushD.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushCSharp.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushCss.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushDiff.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushErlang.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushGolang.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushGroovy.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushInf.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushJavaFX.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushJava.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushJScript.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushPas.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushPerl.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushPhp.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushPlain.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushPowerShell.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushPython.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushRuby.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushRust.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushSass.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushScad.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushBasic.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushVerilog.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushScala.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushSql.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushXml.js"></script>' + '\n');
+            f.write('<script type="text/javascript" src="../scripts/shBrushFortran.js"></script>' + '\n');
+            f.write('\n');
+            f.write('<script type="text/javascript">SyntaxHighlighter.all();</script>' + '\n');
+            f.write('</head>' + '\n');
             f.write('\n');
             f.write('<body>' + '\n');
             f.write('<div id="wrapper">' + '\n');
@@ -50,7 +98,7 @@ def patch_html(path):
             f.write(t + '\n')
 
     if start_write == True:
-        f.write('<br />' + '\n')
+        f.write('<br>' + '\n')
         f.write('</div>' + '\n\n')
         f.write('</div>' + '\n')
         f.write('</div>' + '\n')
