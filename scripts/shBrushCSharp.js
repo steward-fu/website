9886c84718bb2edfc0604cbfc8225c0641fb7451
steward-fu <steward.fu@gmail.com>
2025-12-01 13:33:46 +0800

    C# Fixed Init_libMPSSE hang issue.

--- scripts/shBrushCSharp.js
+++ scripts/shBrushCSharp.js
@@ -1,43 +1,43 @@
 /**
- * SyntaxHighlighter
- * http://alexgorbatchev.com/SyntaxHighlighter
- *
- * SyntaxHighlighter is donationware. If you are using it, please donate.
- * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
- *
- * @version
- * 3.0.83 (July 02 2010)
- * 
- * @copyright
- * Copyright (C) 2004-2010 Alex Gorbatchev.
- *
- * @license
- * Dual licensed under the MIT and GPL licenses.
- */
+* SyntaxHighlighter
+* http://alexgorbatchev.com/SyntaxHighlighter
+*
+* SyntaxHighlighter is donationware. If you are using it, please donate.
+* http://alexgorbatchev.com/SyntaxHighlighter/donate.html
+*
+* @version
+* 3.0.83 (July 02 2010)
+* 
+* @copyright
+* Copyright (C) 2004-2010 Alex Gorbatchev.
+*
+* @license
+* Dual licensed under the MIT and GPL licenses.
+*/
 ;(function() {
- typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-  function Brush() {
-    var my_constant = 'System Console';
-    var my_keyword = 'abstract as base bool break byte case catch char checked class const continue decimal default delegate do double else enum event explicit extern false finally fixed float for foreach get goto if implicit in int interface internal is lock long namespace new null object operator out override params private protected public readonly ref return sbyte sealed set short sizeof stackalloc static string struct switch this throw true try typeof uint ulong unchecked unsafe ushort using virtual void while';
-    var my_type = 'test';
-    var my_function = 'main WriteLine Main';
+function Brush() {
+var my_constant = 'System Console';
+var my_keyword = 'abstract as base bool break byte case catch char checked class const continue decimal default delegate do double else enum event explicit extern false finally fixed float for foreach get goto if implicit in int interface internal is lock long namespace new null object operator out override params private protected public readonly ref return sbyte sealed set short sizeof stackalloc static string struct switch this throw true try typeof uint ulong unchecked unsafe ushort using virtual void while';
+var my_type = 'EventArgs';
+var my_function = 'Init_libMPSSE Main_Form_Load main WriteLine Main';
 
-    this.regexList = [
-      {regex: /^ *#.*/gm, css: 'my_preprocessor'},
-      {regex: SyntaxHighlighter.regexLib.doubleQuotedString, css:'string'},
-      {regex: SyntaxHighlighter.regexLib.singleQuotedString, css:'string'},
-      {regex: SyntaxHighlighter.regexLib.multiLineCComments, css:'my_comment'},
-      {regex: SyntaxHighlighter.regexLib.singleLineCComments, css:'my_comment'},
-      {regex: new RegExp(this.getKeywords(my_type),'gm'), css:'my_type bold'},
-      {regex: new RegExp(this.getKeywords(my_keyword),'gm'), css:'my_keyword bold'},
-      {regex: new RegExp(this.getKeywords(my_constant),'gm'), css:'my_constant bold'},
-      {regex: new RegExp(this.getKeywords(my_function),'gm'), css:'my_function bold'},
-    ];
-  };
+this.regexList = [
+{regex: /^ *#.*/gm, css: 'my_preprocessor'},
+{regex: SyntaxHighlighter.regexLib.doubleQuotedString, css:'string'},
+{regex: SyntaxHighlighter.regexLib.singleQuotedString, css:'string'},
+{regex: SyntaxHighlighter.regexLib.multiLineCComments, css:'my_comment'},
+{regex: SyntaxHighlighter.regexLib.singleLineCComments, css:'my_comment'},
+{regex: new RegExp(this.getKeywords(my_type),'gm'), css:'my_type bold'},
+{regex: new RegExp(this.getKeywords(my_keyword),'gm'), css:'my_keyword bold'},
+{regex: new RegExp(this.getKeywords(my_constant),'gm'), css:'my_constant bold'},
+{regex: new RegExp(this.getKeywords(my_function),'gm'), css:'my_function bold'},
+];
+};
 
-  Brush.prototype  = new SyntaxHighlighter.Highlighter();
-  Brush.aliases  = ['c#', 'csharp'];
-  SyntaxHighlighter.brushes.CSharp = Brush;
-  typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+Brush.prototype  = new SyntaxHighlighter.Highlighter();
+Brush.aliases  = ['c#', 'csharp'];
+SyntaxHighlighter.brushes.CSharp = Brush;
+typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
 })();
